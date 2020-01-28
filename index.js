d5e91dc56f1137e6a619b5190a694143c7276413
mva66 <mva6644@LAPTOP-ETVFKQFE.localdomain>
2020-01-27 20:31:27 -0500

    updated js

--- index.js
+++ index.js
@@ -95,10 +95,7 @@ app.get("/movies/genres/:Name", function(req, res) {
 });
 
 //get list of users
-app.get("/users", passport.authenticate("jwt", { session: false }), function(
-  req,
-  res
-) {
+app.get("/users", function(req, res) {
   Users.find()
     .then(function(users) {
       res.status(201).json(users);
